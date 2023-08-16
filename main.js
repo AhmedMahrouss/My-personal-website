58c9987d2d0996426cc75446e69d860064a34d34
AhmedMahrous <amahrous176@gmail.com>
2023-08-16 04:28:51 +0300

    Upload Projects

---
+++ main.js
@@ -0,0 +1,34 @@
+let icons = document.querySelectorAll("a i");
+
+icons[0].onclick=()=>{
+    icons.forEach(icon=>{
+        icon.classList.remove('active')
+    })
+    icons[0].classList.add('active');
+}
+icons[1].onclick = ()=>{
+    icons.forEach(icon=>{
+        icon.classList.remove('active')
+    })
+    icons[1].classList.add('active');
+}
+icons[2].onclick = ()=>{
+    icons.forEach(icon=>{
+        icon.classList.remove('active')
+    })
+    icons[2].classList.add('active');
+}
+icons[3].onclick = ()=>{
+    icons.forEach(icon=>{
+        icon.classList.remove('active')
+    })
+    icons[3].classList.add('active');
+}
+icons[4].onclick = ()=>{
+    icons.forEach(icon=>{
+        icon.classList.remove('active')
+    })
+    icons[4].classList.add('active');
+}
+
+
