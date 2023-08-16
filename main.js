00f1574ae073dbd6f2b5a50d01a52f2f05fee5d3
AhmedMahrous <amahrous176@gmail.com>
2023-08-16 05:19:01 +0300

    Upload Projects

--- main.js
+++ main.js
@@ -31,4 +31,15 @@ icons[4].onclick = ()=>{
     icons[4].classList.add('active');
 }
 
+let close  = document.querySelector(".close");
+let msg = document.querySelector(".msg");
+let showMsg = document.querySelector(".service .box :nth-child(2)");
 
+showMsg.onclick = ()=>{
+msg.style.display = 'block'
+document.querySelector(".container").style.filter = 'blur(5px)'
+}
+close.onclick = ()=>{
+    msg.style.display = 'none'
+document.querySelector(".container").style.filter = 'blur(0px)'
+}
